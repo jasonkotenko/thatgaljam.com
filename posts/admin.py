fdc4c8d661aca9de0e145bacf3ee6fa544f41891
Jason Kotenko <jasonkotenko@yahoo.com>
2011-03-10 14:23:39 +0800

    TinyMCE added to Posts

--- posts/admin.py
+++ posts/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    class Media:
+        js = ('/media/js/tiny_mce/tiny_mce.js', 
+                '/media/js/tiny_mce/textareas.js',)
+
+admin.site.register(Post, PostAdmin)
