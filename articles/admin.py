ad9584bf9802473f43bff903707fe7365fbab7eb
Jason Kotenko <kotenko@gmail.com>
2011-03-11 10:27:40 +0000

    Various changes - tinymce, jam's template changes

--- articles/admin.py
+++ articles/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from models import Article
 
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+  class Media:
+    js = ('/media/js/tiny_mce/tiny_mce.js',
+      '/media/js/tiny_mce/textareas.js',)
+
+admin.site.register(Article, ArticleAdmin)
