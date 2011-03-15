c73ea53a05c4d2ffb01c16384d827fa0fb1ce57e
Jason Kotenko <kotenko@gmail.com>
2011-03-15 07:25:08 +0000

    got rid of "cc myself"

--- contact/forms.py
+++ contact/forms.py
@@ -5,5 +5,5 @@ class ContactForm(forms.Form):
     subject = forms.CharField(max_length=200)
     sender = forms.EmailField(label='Your e-mail')
     message = forms.CharField(widget=Textarea())
-    cc_myself = forms.BooleanField(required=False)
+    #cc_myself = forms.BooleanField(required=False)
     
--- contact/views.py
+++ contact/views.py
@@ -11,12 +11,12 @@ def contact(request):
             subject = form.cleaned_data['subject']
             message = form.cleaned_data['message']
             sender = form.cleaned_data['sender']
-            cc_myself = form.cleaned_data['cc_myself']
+            #cc_myself = form.cleaned_data['cc_myself']
             subject = "[thatgaljam] Contact Form - " + subject
             
             recipients = ['jamglam@gmail.com']
-            if cc_myself:
-                recipients.append(sender)
+            #if cc_myself:
+            #    recipients.append(sender)
                 
             from django.core.mail import send_mail
             send_mail(subject, message, sender, recipients)
